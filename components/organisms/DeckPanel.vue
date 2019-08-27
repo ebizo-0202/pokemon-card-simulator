7debae7a17c2a3d2e46aa3937ac38a92e858e21e
yuki ichikawa <y.icchii.0618@gmail.com>
2019-08-28 03:50:47 +0900

    デッキ選択画面のモックを作成

---
+++ components/organisms/DeckPanel.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  v-card
+    v-container(fill-height fluid)
+      v-layout(fill-height)
+        v-flex(xs12 align-end flexbox)
+          span(class="headline") ジラーチサンダー
+    v-card-title
+      div
+        span(class="grey--text") いっちー
+    v-card-actions
+      v-btn(class="ma-2" tile color="indigo" dark) このデッキを使う
+      v-btn(class="ma-2" tile outlined color="success") #[v-icon(left) mdi-pencil] 編集
+</template>
