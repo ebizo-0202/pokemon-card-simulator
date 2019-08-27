13410aa06526ec5974f11d6e7f947d7e2ef8acfe
市川　夕貴 <yichikawa@PA56.local>
2019-08-28 03:50:47 +0900

    デッキ選択画面のモックを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,22 @@
 <template lang="pug">
   .deck
-    p デッキ一覧画面
     v-app
       v-content
+        p.display-2 デッキ一覧画面
         v-btn(color="primary") 新しいデッキを追加
         v-layout
           v-flex(xs12 sm6 offset-sm3)
-            v-card
-              v-container(fill-height fluid)
-                v-layout(fill-height)
-                  v-flex(xs12 align-end flexbox)
-                    span(class="headline") Top 10 Australian beaches
-              v-card-title
-                div
-                  span(class="grey--text") Number 10
-            v-card-actions
-              v-btn(text color="orange") このデッキを使う
-              v-btn(test color="orange") 編集
+            DeckPanel
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
 
-@Component
+@Component({
+  components: {
+    DeckPanel: () => import('@/components/organisms/DeckPanel.vue')
+  }
+})
 export default class TopPage extends Vue {
 }
 </script>
